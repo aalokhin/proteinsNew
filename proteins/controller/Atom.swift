df35c0f3007480849211170074516a8102b32244
aalokhin <n.alokhina@gmail.com>
2019-07-24 15:43:08 +0300

    added al color schemes

--- proteins/controller/Atom.swift
+++ proteins/controller/Atom.swift
@@ -37,6 +37,8 @@ class Atom {
         return SCNVector3Make(self.x, self.y, self.z)
     }
     
+    
+    /* https://www.umass.edu/microbio/chime/beta/ben-tal/s180/cpk-rgb.htm */
     func getCPKColor(el : String) -> UIColor {
         switch el {
         case "C":
@@ -51,7 +53,59 @@ class Atom {
             return UIColor(red: 1, green: 0.7843, blue: 0.1961, alpha: 1.0) /* #ffc832 */
         case "Na":
             return UIColor(red: 0, green: 0, blue: 1, alpha: 1.0) /* #0000ff */
-         default:
+        case "Cl":
+            return UIColor(red: 0, green: 1, blue: 0, alpha: 1.0) /* #00ff00 */
+        case "B":
+             return UIColor(red: 0, green: 1, blue: 0, alpha: 1.0) /* #00ff00 */
+ /* Ca, Mn, Al, Ti, Cr, Ag    dark grey        [128,128,144]  80 80 90  */
+        case "Ca":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+        case "Mn":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+        case "Al":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+        case "Ti":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+        case "Cr":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+        case "Ag":
+            return UIColor(red: 0.502, green: 0.502, blue: 0.5647, alpha: 1.0) /* #808090 */
+/*     Zn, Cu, Ni, Br            brown            [165,42,42]    A5 2A 2A */
+        case "Zn":
+            return UIColor(red: 0.6471, green: 0.1647, blue: 0.1647, alpha: 1.0) /* #a52a2a */
+        case "Cu":
+            return UIColor(red: 0.6471, green: 0.1647, blue: 0.1647, alpha: 1.0) /* #a52a2a */
+        case "Ni":
+            return UIColor(red: 0.6471, green: 0.1647, blue: 0.1647, alpha: 1.0) /* #a52a2a */
+        case "Br":
+            return UIColor(red: 0.6471, green: 0.1647, blue: 0.1647, alpha: 1.0) /* #a52a2a */
+/*     F, Si, Au                 goldenrod        [218,165,32]   DA A5 20 */
+        case "F":
+            return UIColor(red: 0.8549, green: 0.6471, blue: 0.1255, alpha: 1.0) /* #daa520 */
+        case "Si":
+            return UIColor(red: 0.8549, green: 0.6471, blue: 0.1255, alpha: 1.0) /* #daa520 */
+        case "Au":
+            return UIColor(red: 0.8549, green: 0.6471, blue: 0.1255, alpha: 1.0) /* #daa520 */
+/* Iodine                    purple           [160,32.240]   A020F0 */
+        case "I":
+            return UIColor(red: 0.6275, green: 0.1255, blue: 0.9412, alpha: 1.0) /* #a020f0 */
+//            Lithium                   firebrick        [178,34,34]    B22222
+        case "Li":
+            return UIColor(red: 0.698, green: 0.1333, blue: 0.1333, alpha: 1.0) /* #b22222 */
+//            Helium                    pink             [255,192,203]  FFC0CB
+        case "He":
+            return UIColor(red: 1, green: 0.7529, blue: 0.7961, alpha: 1.0) /* #ffc0cb */
+//            Magnesium                 forest green     [34,139,34]    228B22
+        case "Mg":
+            return UIColor(red: 0.1333, green: 0.5451, blue: 0.1333, alpha: 1.0) /* #228b22 */
+//            Phosphorus, Iron, Barium  orange           [255,165,0]    FFA500
+        case "P":
+            return UIColor(red: 1, green: 0.6471, blue: 0, alpha: 1.0) /* #ffa500 */
+        case "Fe":
+            return UIColor(red: 1, green: 0.6471, blue: 0, alpha: 1.0) /* #ffa500 */
+        case "Ba":
+            return UIColor(red: 1, green: 0.6471, blue: 0, alpha: 1.0) /* #ffa500 */
+        default:
             return UIColor(red: 1, green: 0.0784, blue: 0.5765, alpha: 1.0) /* #ff1493 */
         }
     }
